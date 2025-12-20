6f816d6b72faf11fb6dab98ae2df7a41e0cc5467
Aulkhami <48144314+Aulkhami@users.noreply.github.com>
2025-12-20 14:53:39 +0700

    Create Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section id="home" className="bg-gray-100 py-12 px-12">
+      <div className="max-w-4xl mx-auto flex flex-col">
+        <h1 className="text-5xl font-bold text-gray-800">Rakha</h1>
+      </div>
+    </section>
+  );
+};
