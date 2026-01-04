3b4d6da373274bd0bd1de9567a32237b8b6a4892
Aulkhami <48144314+Aulkhami@users.noreply.github.com>
2026-01-04 14:54:29 +0700

    Uh

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,20 @@ import React from "react";
 
 export const Hero = () => {
   return (
-    <section id="home" className="bg-gray-100 py-12 px-12">
-      <div className="max-w-4xl mx-auto flex flex-col">
-        <h1 className="text-5xl font-bold text-gray-800">Rakha</h1>
+    <section
+      id="home"
+      className="bg-gray-100 text-gray-800 py-12 px-12 flex justify-around"
+    >
+      <div className="flex flex-col justify-center">
+        <h1 className="text-5xl font-bold">Rakha</h1>
+        <p>Hi! I am Aulia Rakha Ilhami</p>
+      </div>
+      <div className="flex justify-center items-center">
+        <img
+          src="/me.jpg"
+          width={512}
+          className="w-32 h-32 rounded-full object-cover object-top border-2 shadow-lg shadow-fuchsia-500"
+        />
       </div>
     </section>
   );
