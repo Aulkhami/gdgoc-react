0540cd306496ff1badf166786174fa523ad9c8d4
Aulkhami <rakha.tblt@gmail.com>
2026-01-30 05:47:32 +0700

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,24 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-0 h-16 left-0 w-screen flex bg-gray-800 items-center px-4 py-4">
+    <nav className="text-slate-950 font-bold fixed top-0 h-24 left-0 w-screen flex bg-transparent items-center px-4 py-4">
       <div className="max-w-4xl mx-auto flex justify-between items-center gap-6 h-full">
         <div className="flex gap-6 h-full">
           <a
             href="#home"
-            className="hover:bg-gray-50/20 delay-50 ease-out transition h-full rounded-md flex justify-center items-center px-4"
+            className="hover:scale-110 delay-50 ease-out transition h-full rounded-md flex justify-center items-center px-4"
           >
             Home
           </a>
           <a
             href="#skills"
-            className="hover:bg-gray-50/20 delay-50 ease-out transition h-full rounded-md flex justify-center items-center px-4"
+            className="hover:scale-110 delay-50 ease-out transition h-full rounded-md flex justify-center items-center px-4"
           >
             Skills
           </a>
           <a
             href="#contact"
-            className="hover:bg-gray-50/20 delay-50 ease-out transition h-full rounded-md flex justify-center items-center px-4"
+            className="hover:scale-110 delay-50 ease-out transition h-full rounded-md flex justify-center items-center px-4"
           >
             Contact
           </a>
