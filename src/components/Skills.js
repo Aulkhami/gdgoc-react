8d6ed036b0baea0966fe5de2d8f2604e272abb5c
Aulkhami <rakha.tblt@gmail.com>
2026-01-31 19:58:14 +0700

    Skills animation

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as motion from "motion/react-client";
 
 const mySkills = [
   {
@@ -49,17 +50,30 @@ export default function Skills() {
       <h2 className="font-extrabold text-2xl text-center mb-12">My Skills</h2>
       <div className="flex flex-wrap justify-center items-center gap-8">
         {mySkills.map((v, i) => (
-          <div key={i} className="h-12 w-64">
+          <motion.div
+            key={i}
+            className="h-12 w-64"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.25 * i }}
+          >
             <h6 className="font-bold font-mono">{v.skill}</h6>
             <div className="h-2 rounded-full flex bg-slate-950 shadow-md shadow-violet-950">
-              {Array.from({ length: 5 }, (_, i) => (
-                <div
-                  key={i}
-                  className={`h-full w-auto grow ${i == 0 ? "rounded-l-full" : ""} ${i == 4 ? "rounded-r-full" : ""} ${v.level > i ? "bg-violet-500" : "bg-cyan-300/10"}`}
-                ></div>
+              {Array.from({ length: 5 }, (_, j) => (
+                <motion.div
+                  key={j}
+                  className={`h-full w-auto grow ${j == 0 ? "rounded-l-full" : "-ml-px"} ${j == 4 ? "rounded-r-full" : ""} ${v.level > j ? "bg-violet-500" : "bg-cyan-300/10"}`}
+                  initial={{ clipPath: "inset(0 100% 0 0)" }}
+                  whileInView={{ clipPath: "inset(0 0% 0 0)" }}
+                  transition={{
+                    duration: 0.1,
+                    delay: 0.25 + 0.25 * i + 0.1 * j,
+                  }}
+                  viewport={{ once: true }}
+                ></motion.div>
               ))}
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </section>
